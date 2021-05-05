f7c4d4df13a473cb13c6834703cc1e5603721126
Joaquin Arias Simone <juaco1993@gmail.com>
2021-05-05 18:57:44 -0300

    Agregado mapa en paso 3 e inputs

--- resources/views/empresas/registro/paso3.blade.php
+++ resources/views/empresas/registro/paso3.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 @section('content')
     <h1>Paso 3</h1>
+    <h3>Domicilio de la Actividad</h3>
     @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
@@ -12,9 +13,16 @@
     @endif
      <form action="/registro4" method="POST">
         @csrf
-            <input type="text" name="name" class="form-controll" placeholder="Ingrese nombre" value="{{ session()->get('registro.nombre') }}">
+            <h4>Latitud</h4>
+            <input type="text" name="latitud" id="latitud" class="form-controll" placeholder="Domicilio Actividad: Latitud" value="{{ session()->get('empresa.latitud') }}">
+            <h4>Longitud</h4>
+            <input type="text" name="longitud" id="longitud" class="form-controll" placeholder="Domicilio Actividad: Longitud" value="{{ session()->get('empresa.longitud') }}">
+
+            <br><br><br>
+            <p>Marcá la ubicación haciendo click en el mapa:</p>
+            <div id="map" style="position: static; width: 400px; height: 400px"></div>
+
             <a type="button" href="/registro2" class="btn btn-warning">Vuelta al paso 2</a>
             <button type="submit" class="btn btn-primary">Continuar</button>
      </form>
-     <div id="map"></div>
 @endsection
